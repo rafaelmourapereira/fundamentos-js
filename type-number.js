8d85e18d0fef7e4e0052ae28a4617604428fd1f4
rafaelmourapereira <rafael.moura.pereira@escola.pr.gov.br>
2026-03-06 11:28:08 -0300

    Add files via upload

---
+++ type-number.js
@@ -0,0 +1,25 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematíca = primeiroNumero + segundoNumero
+
+console.log(operacaoMatematíca);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+//naN -> Not a Number (não é numero)
+
+const alura = "Alura";
+
+console.log(alura * primeiroNumero);
