f8575f8927820dc11ce9a039f1c941f4c15314ca
rafaelmourapereira <rafael.moura.pereira@escola.pr.gov.br>
2026-03-31 10:33:42 -0300

    parametros

--- parametros.js
+++ parametros.js
@@ -5,3 +5,9 @@ function soma(numero1, numero2){
 console.log(soma( 2, 2));
 console.log(soma( 245, 28));
 console.log(soma( -500, 60));
+
+             function nomeIdade(nome, idade){
+                return meu nome e >{nome} e minha idade é${idade}
+}
+
+console.log(nomeIdade{"Alexandre", 44})
