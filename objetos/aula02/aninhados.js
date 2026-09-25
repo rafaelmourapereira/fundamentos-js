5a5f82ef28c0be985e07cf657d09d1cb88739a28
rafaelmourapereira <rafael.moura.pereira@escola.pr.gov.br>
2026-09-25 08:39:47 -0300

    indereço

---
+++ objetos/aula02/aninhados.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome: "Rafael",
+    idade: 18,
+    email: "rafael@firma.com",
+    telefone: ["3326500914", "33589011453"],
+};
+
+cliente.endereco; ={
+    rua: "R. Dr Orlando Araujo Costa",
+    numero: 1931,
+    apartamento: true,
+    complemento: "ap 934",
+};
+
